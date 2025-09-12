1369223e68ee0a40280dc3591666d0fad5513104
Andy Yip <andy.yip@oocl.com>
2025-09-12 14:58:01 +0800

    feat: implemented fetch todo item text with context

---
+++ src_ts/reducers/todoReducer.tsx
@@ -0,0 +1,9 @@
+export const initialState = [
+  {id: 1, text: "the first todo", done: false},
+  {id: 2, text: "the second todo", done: false},
+];
+
+// reducer is a pure function that define and gather all state update logic
+export const todoReducer = (state: typeof initialState, action: {type: string, payload?: any}) => {
+  return state;
+};
